3ae1f0f8e512e4b6760c12148dc965d4feac13df
belie <belie@Vishwash>
2022-10-15 18:24:51 +0530

    initial commit

---
+++ src/main/java/com/example/vocabulary/VocabularyFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.vocabulary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabularyFinderApplication.class, args);
+	}
+
+}
